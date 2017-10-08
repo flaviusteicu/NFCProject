43646a9e6a7677eb252a3cbebd254b40d9a61b1d
flaviusteicu <flavius_andrei06@yahoo.com>
2017-10-08 19:59:46 +0300

    SecondCommit

---
+++ app/src/main/java/com/example/flavius/nfcproject/UnFragment.java
@@ -0,0 +1,47 @@
+package com.example.flavius.nfcproject;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UnFragment extends DialogFragment
+{
+
+
+    public UnFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View nextReturn = inflater.inflate(R.layout.fragment_un, container, false);
+        ButterKnife.bind(this, nextReturn);
+        return nextReturn;
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Show soft keyboard automatically and request focus to field
+
+    }
+}
